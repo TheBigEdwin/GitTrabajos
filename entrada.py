046a2963ea008d3fbaa4dc79ed989970475e5658
Edwin <thebigeddie064@gmail.com>
2023-05-29 21:36:29 -0400

    Archivo Python

---
+++ entrada.py
@@ -0,0 +1,16 @@
+nombre_completo = input('ingresa tu numbre completo:') # str
+
+edad = input ('ingresa tu edad:')
+edad = int (edad)
+
+
+altura = input('ingresa tu altura:')
+altura = float (altura)
+
+
+autorizacion = input('¿Autoraizas el programa? (si/no)')
+autorizacion = autorizacion == 'si'
+
+print(autorizacion)
+
+
